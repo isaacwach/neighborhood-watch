380da2553c3bc0f328f66446325101604a49229c
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-15 19:40:53 +0300

    imported models, User class, post_save and receiver in models.py file

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
