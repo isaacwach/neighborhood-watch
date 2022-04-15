1aed03a3ff89daff157d4f934423d8dcd294290b
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-15 20:16:10 +0300

    created logout view

--- hood/views.py
+++ hood/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render, HttpResponseRedirect, redirect, get_object_
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout as django_logout
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_protect
 from django.contrib import messages
-
+from .forms import *
+from .models import *
 # Create your views here.
 
 def index(request):
     return render(request,"index.html")
+
+@login_required(login_url='/accounts/login/')
+@csrf_protect
+def logout(request):
+    django_logout(request)
+    return  HttpResponseRedirect('/')
