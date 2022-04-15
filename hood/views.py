83bdfdb139293b655c0156446da761cba128432c
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-15 16:55:00 +0300

    created index view

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, redirect, get_object_or_404
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout as django_logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_protect
+from django.contrib import messages
 
 # Create your views here.
+
+def index(request):
+    return render(request,"index.html")
