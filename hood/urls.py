c4880d686f3734b6057eedefc38b2c0a99b55338
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-16 19:05:39 +0300

    resolved bugs

--- hood/urls.py
+++ hood/urls.py
@@ -9,11 +9,11 @@ urlpatterns=[
     path('logout', views.logout, name='logout'),
     path('all-hoods/',views.neighbourhoods,name='neighbourhood'),
     path('new-hood/', views.create_neighbourhood, name='new-hood'),
-     path('join_hood/<id>', views.join_neighbourhood, name='join-hood'),
+    path('join_hood/<id>', views.join_neighbourhood, name='join-hood'),
     path('leave_hood/<id>', views.leave_neighbourhood, name='leave-hood'),
     path('single_hood/<hood_id>', views.single_neighbourhood, name='single-hood'),
     path('<hood_id>/post/', views.create_post, name='post'),
-     path('search/', views.search_business, name='search'),
+    path('search/', views.search_business, name='search'),
 ]
 
 if settings.DEBUG:
